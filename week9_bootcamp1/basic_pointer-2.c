89e129c72693935ad63edd6897ee62223127d5e0
Oliver Plant <sc23op@leeds.ac.uk>
2023-11-28 14:38:40 +0000

    Add week 9 session 1 work

---
+++ week9_bootcamp1/basic_pointer-2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int lets_get_serious()
+{
+    printf("Function run.\n");
+    return 0;
+}
+
+int main()
+{
+    printf("\n \n Even functions have address. \n \n");
+
+    printf("The address of main function = %p \n \n", (void *)main);
+
+    printf("The address of lets_get_serious function = %p \n \n", (void *) lets_get_serious);
+
+    lets_get_serious();
+
+    unsigned int bytepatch = 0x00;
+    unsigned char* func = (unsigned char*)&lets_get_serious;
+    printf("%u\n", *func);
+    *func = (unsigned char)0xC3;
+    printf("%u\n", *func);
+
+    lets_get_serious();
+}
